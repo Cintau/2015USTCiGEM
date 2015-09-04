ed95b30ccec8a7080a6fdc72270ec33a3fb57371
Tao <526784005@qq.com>
2015-09-04 23:13:14 +0800

    Reup

--- act.m
+++
@@ -1,5 +0,0 @@
-function b=act(I1,I2)
-G=I1-I2;%compare two of nearby sequential images
-b=shujun2(G);
-
-
--- mulshu.m
+++
@@ -1,5 +0,0 @@
-function A=mulshu(m)
-A=zeros(m,1);
-for i=1:m
-    A(i,1)=shujun(i);%read a series of images
-end
